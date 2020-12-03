a20004d16134a39eae21ba9366441e60e0722b25
David <david.edelstein@gmail.com>
2020-12-03 18:24:23 -0500

    first commit

---
+++ material.inc.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *------
+ * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
+ * TrickOfTheRails implementation : @ David Edelstein <davidedelstein@gmail.com>
+ * 
+ * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
+ * See http://en.boardgamearena.com/#!doc/Studio for more information.
+ * -----
+ *
+ * material.inc.php
+ *
+ * TrickOfTheRails game material description
+ *
+ * Here, you can describe the material of your game with PHP variables.
+ *   
+ * This file is loaded in your game logic class constructor, ie these variables
+ * are available everywhere in your game logic code.
+ *
+ */
+
+
+/*
+
+Example:
+
+$this->card_types = array(
+    1 => array( "card_name" => ...,
+                ...
+              )
+);
+
+*/
+
+
+
+
---
+++ trickoftherails.action.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ *------
+ * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
+ * TrickOfTheRails implementation : @ David Edelstein <davidedelstein@gmail.com>
+ *
+ * This code has been produced on the BGA studio platform for use on https://boardgamearena.com.
+ * See http://en.doc.boardgamearena.com/Studio for more information.
+ * -----
+ * 
+ * trickoftherails.action.php
+ *
+ * TrickOfTheRails main action entry point
+ *
+ *
+ * In this file, you are describing all the methods that can be called from your
+ * user interface logic (javascript).
+ *       
+ * If you define a method "myAction" here, then you can call it from your javascript code with:
+ * this.ajaxcall( "/trickoftherails/trickoftherails/myAction.html", ...)
+ *
+ */
+  
+  
+  class action_trickoftherails extends APP_GameAction
+  { 
+    // Constructor: please do not modify
+   	public function __default()
+  	{
+  	    if( self::isArg( 'notifwindow') )
+  	    {
+            $this->view = "common_notifwindow";
+  	        $this->viewArgs['table'] = self::getArg( "table", AT_posint, true );
+  	    }
+  	    else
+  	    {
+            $this->view = "trickoftherails_trickoftherails";
+            self::trace( "Complete reinitialization of board game" );
+      }
+  	} 
+  	
+  	// TODO: defines your action entry points there
+
+
+    /*
+    
+    Example:
+  	
+    public function myAction()
+    {
+        self::setAjaxMode();     
+
+        // Retrieve arguments
+        // Note: these arguments correspond to what has been sent through the javascript "ajaxcall" method
+        $arg1 = self::getArg( "myArgument1", AT_posint, true );
+        $arg2 = self::getArg( "myArgument2", AT_posint, true );
+
+        // Then, call the appropriate method in your game logic, like "playCard" or "myAction"
+        $this->game->myAction( $arg1, $arg2 );
+
+        self::ajaxResponse( );
+    }
+    
+    */
+
+  }
+  
+
